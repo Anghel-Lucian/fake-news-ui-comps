750d4fd1f682adac6495eaac33d0f697d17cbb04
Lucian Anghel <anghelpaul99@gmail.com>
2021-10-25 11:03:23 +0300

    added preliminary routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { UserRegister, Button } from "./components";
 
 const App = () => {
   return (
-    <div className="App">
-      <UserRegister />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        {/* <NavBar /> */}
+        <div className="main-view">
+          <Switch>
+            <Route path="/register" exact component={UserRegister} />
+            {/* <Route path="/history" exact component={SearchHistory} />
+            <Route path="/truth-score" exact component={TruthScore} /> */}
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/appSlice.js
+++ src/appSlice.js
@@ -15,7 +15,8 @@ const appSlice = createSlice({
       // send req to backend, log user in, update state, components update themselves accordingly
     },
     sendTweet(state, action) {
-      // send req to backend with ID or URL, await verdict
+      // send req to backend with ID or URL, await verdict, components update themselves accordingly
+      // NOTE: this needs to be an async thunk
     },
   },
 });
